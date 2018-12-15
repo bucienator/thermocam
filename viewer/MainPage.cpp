6a978fe79e0beabf9bd58f509b0b207780709335
Peter Bolla <buci@metacortex.hu>
2018-12-15 22:32:51 +0100

    A stub for an UWP app reading the thermo images

---
+++ viewer/MainPage.cpp
@@ -0,0 +1,67 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+
+using namespace winrt;
+using namespace Windows::UI;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Automation::Peers;
+using namespace Windows::UI::Xaml::Media;
+
+namespace winrt::viewer::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+		NotifyUser(L"", NotifyType::StatusMessage);
+    }
+
+	void MainPage::NotifyUser(const std::wstring & strMessage, const NotifyType type)
+	{
+		// If called from the UI thread, then update immediately.
+		// Otherwise, schedule a task on the UI thread to perform the update.
+		if (Dispatcher().HasThreadAccess())
+		{
+			UpdateStatus(strMessage, type);
+		}
+		else
+		{
+			Dispatcher().RunAsync(CoreDispatcherPriority::Normal, [=]() {UpdateStatus(strMessage, type); });
+		}
+	}
+
+	void MainPage::UpdateStatus(const std::wstring & strMessage, const NotifyType type)
+	{
+		switch (type)
+		{
+		case NotifyType::StatusMessage:
+			StatusBorder().Background(SolidColorBrush(Colors::Green()));
+			break;
+		case NotifyType::ErrorMessage:
+			StatusBorder().Background(SolidColorBrush(Colors::Red()));
+			break;
+		}
+
+		StatusBlock().Text(strMessage);
+
+		// Collapse the StatusBlock if it has no text to conserve real estate.
+		if (StatusBlock().Text() != L"")
+		{
+			StatusBorder().Visibility(Visibility::Visible);
+			StatusPanel().Visibility(Visibility::Visible);
+		}
+		else
+		{
+			StatusBorder().Visibility(Visibility::Collapsed);
+			StatusPanel().Visibility(Visibility::Collapsed);
+		}
+
+		// Raise an event if necessary to enable a screen reader to announce the status update.
+		const auto peer = FrameworkElementAutomationPeer::FromElement(StatusBlock());
+		if (peer != nullptr)
+		{
+			peer.RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
+		}
+	}
+
+}
---
+++ viewer/MainPage.h
@@ -0,0 +1,37 @@
+﻿//
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::viewer::implementation
+{
+	enum class NotifyType
+	{
+		StatusMessage,
+		ErrorMessage,
+	};
+
+
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+		void NotifyUser(const std::wstring & strMessage, NotifyType type);
+
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+
+	private:
+		void UpdateStatus(const std::wstring & strMessage, NotifyType type);
+    };
+}
+
+namespace winrt::viewer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
